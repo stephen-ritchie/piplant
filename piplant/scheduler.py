0507040597e291096ae6414274883bcfb3a0f250
Stephen Ritchie <stephenritchie@Stephens-MacBook-Pro.local>
2020-12-04 12:18:17 -0500

    Removed hard coded hostname and added logging.

--- piplant/scheduler.py
+++ piplant/scheduler.py
@@ -1,9 +1,7 @@
-import logging
 import json
 from urllib.parse import urlparse
 
 import requests
-from flask import jsonify
 
 import piplant.lib as lib
 
@@ -11,22 +9,23 @@ import piplant.lib as lib
 class Scheduler:
     def __init__(self, app):
         self.app = app
+        self.api_version = "v1"  # TODO: Make this configurable.
+        self.client_base_url = self.app.config['CLIENT_BASE_URL']
 
     def update(self):
+        url = "{}/api/{}/requests".format(self.app.config['CLIENT_BASE_URL'], self.api_version)
         with self.app.app_context():
             for user in lib.get_users():
                 tasks = lib.get_tasks(user.id)
-                url = "http://0.0.0.0:5001/api/v1/tasks"
                 self.send_tasks_to_client(tasks, url)
 
     def send_tasks_to_client(self, tasks, url):
-        print("Sending {} tasks to {}".format(len(tasks), url))
-        print(tasks)
         url = urlparse(url).geturl()
         try:
             headers = {'content-type': 'application/json'}
             response = requests.post(url=url, data=json.dumps(tasks), headers=headers)
             response.raise_for_status()
+            self.app.logger.debug("Sent %s task(s) to %s" % (len(tasks), url))
         except Exception as err:
-            logging.error(str(err))
+            self.app.logger.error("Could not POST tasks to %s" % url)
 
