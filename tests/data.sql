c676daf14a306907c3c13786d3cdd3d201c780fb
Stephen Ritchie <stephenritchie@Stephens-MacBook-Pro.local>
2020-12-05 15:30:50 -0500

    fix test data

--- tests/data.sql
+++ tests/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (email, password)
+INSERT INTO user (name, email, password, admin)
 VALUES
-  ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+  ('test', 'test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', FALSE),
+  ('test', 'other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', FALSE);
