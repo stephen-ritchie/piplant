7375f4b497c525a5300b326f2ff8c158c838d402
Stephen Ritchie <stephenritchie@Stephens-MacBook-Pro.local>
2020-12-15 11:10:53 -0500

    UI update

--- piplant/views/user.py
+++ piplant/views/user.py
@@ -1,26 +1,32 @@
 import logging
 
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required, current_user
+from flask_login import login_required
 
-from piplant.views.api import schemas
 import piplant.lib as lib
+import piplant.messages as messages
 
 user = Blueprint('user', __name__)
 
 
-@user.route('/user', methods=['GET', 'POST'])
+@user.route('/users/<int:user_id>', methods=['GET', 'POST'])
 @login_required
-def about():
+def about(user_id):
+    _user = lib.get_user(user_id)
+    if _user is None:
+        flash(messages.USER_NOT_FOUND)
+        return redirect(url_for("home.landing"))
+
     if request.method == 'POST':
+        # TODO: Schema validation
         name = request.form.get("name")
         email = request.form.get("email")
         phone = request.form.get("phone")
         try:
-            lib.update_user(current_user.id, name=name, email=email, phone=phone)
+            lib.update_user(_user.id, name=name, email=email, phone=phone)
             return redirect(url_for("home.landing"))
         except Exception as err:
             logging.error(str(err))
             flash(str(err))
 
-    return render_template('user/about.html', user=current_user)
+    return render_template('user/about.html', user=_user)
